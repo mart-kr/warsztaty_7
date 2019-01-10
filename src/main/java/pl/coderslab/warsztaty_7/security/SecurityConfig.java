d705c93ef5dc310a080da6a9452a0c343706ba8b
Maksym Szymczak <szymczak.maksym@gmail.com>
2019-01-10 23:53:30 +0100

    Error page view, moved views to resources/templates

--- src/main/java/pl/coderslab/warsztaty_7/security/SecurityConfig.java
+++ src/main/java/pl/coderslab/warsztaty_7/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests().antMatchers("/add-admin", "/home", "/home/**").permitAll()
+                .authorizeRequests().antMatchers("/add-admin", "/home", "/home/**", "/exception").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/**").authenticated()
                 .and().formLogin()
